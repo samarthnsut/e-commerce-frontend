7fd5057c5b35456b767794a3fbb62698e3676a5b
samarth <45564142+samarthnsut@users.noreply.github.com>
2019-08-10 18:46:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 
-var div4 = document.getElementById('div4');
+$(".category").hide();
 
 
-div4.addEventListener('click',function(){
-    alert('page change confirmation');
-})
+
+var menu= document.querySelectorAll("#navbar a");
+
+for(var i=0;i<menu.length;i++)
+{
+    $(menu[i]).click(function(event){
+        event.preventDefault();
+        targetID=this.textContent.trim().toLowerCase()
+        temp = targetID.split(" ")
+        targetID=temp[0];
+        var target=document.getElementById(targetID)
+        console.log(target)
+         
+        if(targetID=="men")
+        $("#men").show();
+
+
+        var interval=setInterval(function(){
+            var cord = target.getBoundingClientRect()
+            if(cord.top<=0)
+            {clearInterval(interval)
+            return;
+        }
+        window.scrollBy(0,50);
+        },50)
+        
+    })
+}
+
+
+
+
